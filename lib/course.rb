2ef7f56611b5b05600cb9b2df6aa5a599ac435d8
elsa chen <snowing222@gmail.com>
2020-06-04 20:56:43 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,17 +1,29 @@
+require "pry"
 class Course
   attr_accessor :title, :schedule, :description
   @@all=[]
 
+
   def initialize
     @@all<<self
+
   end
+
+
+
+
 
   def self.all
     @@all
+    binding.pry
   end
+
+
 
   def self.reset_all
     @@all.clear
+
   end
 
 end
+Course.all
